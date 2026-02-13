af90d637ffe5f12f2b9b065781c780d54bf25269
Jason Asindi <122324649+jasonasindi@users.noreply.github.com>
2026-02-12 21:32:08 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/for-jazzmeyn/",
   server: {
     host: "::",
     port: 8080,
